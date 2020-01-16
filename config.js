c42964c147d0f7af5b65f36a36006ab0c68e9e1a
happyjy <okwoyjy@gmail.com>
2020-01-16 23:35:17 +0900

    init blog

--- config.js
+++ config.js
@@ -1,28 +1,28 @@
 module.exports = {
   /** Site MetaData (Required all)*/
-  title: `Title`,                           // (* Required)
-  description: `Blog Description`,          // (* Required)
-  author: `Author`,                         // (* Required)
-  siteUrl: 'https://',                      // (* Required)
+  title: `DEV.HAPPY JYOON`,                         // (* Required)
+  description: `Dev.jYoon Blog`,          // (* Required)
+  author: `jYoon`,                         // (* Required)
+  siteUrl: 'https://happyjy.github.io',                      // (* Required)
     // ex.'https://junhobaik.github.io'
     // ex.'https://junhobaik.github.io/' << X, Do not enter "/" at the end.
 
   /** Header */
-  profileImageFileName: '', // include filename extension ex.'profile.jpg'
+  profileImageFileName: 'profile.jpg', // include filename extension ex.'profile.jpg'
     // The Profile image file is located at path "./images/"
     // If the file does not exist, it is replaced by a random image.
 
   /** Home > Bio information*/
-  comment: 'Enter your information in the "./config.js" file. ',
-  name: 'User Name',
+  comment: 'Jr. Web Front-end Developer. /javascript, react, node.js ... ',
+  name: 'Jaeyoon Yoon',
   company: '',
   location: 'Korea',
-  email: '',
-  website: 'https://github.com/junhobaik/junhobaik.github.io',           // ex.'https://junhobaik.github.io'
+  email: 'okwoyjy@gmail.com',
+  website: 'https://happyjy.github.io',           // ex.'https://junhobaik.github.io'
   linkedin: '',                                                          // ex.'https://www.linkedin.com/in/junho-baik-16073a19ab'
   facebook: '',                                                          // ex.'https://www.facebook.com/zuck' or 'https://www.facebook.com/profile.php?id=000000000000000'
-  instagram: '',                                                         // ex.'https://www.instagram.com/junhobaik'
-  github: '',                                                            // ex.'https://github.com/junhobaik'
+  instagram: 'https://www.instagram.com/happyjy',                                                         // ex.'https://www.instagram.com/junhobaik'
+  github: 'https://github.com/happyjy',                                                            // ex.'https://github.com/junhobaik'
 
   /** Post */
   enablePostOfContents: true,     // TableOfContents activation (Type of Value: Boolean. Not String)
